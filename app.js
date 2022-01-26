4bde13988e5b23dfbeb42d9b8c248806f6bc154b
Herus Macedo <herus2019@hotmail.com>
2022-01-25 23:23:30 -0300

    Pastar Organizadas

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+const mongoose = require('mongoose');
+const linkRoute = require("./routes/linkRoute")
+const Link = require("./models/Link")
+
+
+mongoose.connect('mongodb://localhost/links', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+})
+
+let db = mongoose.connection;
+
+db.on("error", () => { console.log("houve um erro") })
+db.once("open", () => {
+    console.log("Banco carregado")
+
+})
+
+app.use("/", linkRoute)
+
+app.listen(PORT, () => {
+
+    console.log("Server Runing...")
+
+});
---
+++ controllers/linkController.js
@@ -0,0 +1,24 @@
+
+const Link = require("../models/Link");
+
+const redirect = async (req,res) =>{
+
+    let title = req.params.title;
+
+    try {
+
+        let doc = await Link.findOne({ title })
+
+        console.log(doc)
+
+        res.redirect(doc.url)
+
+    } catch (error) {
+
+        res.send("Houve um erro");
+
+    }
+    
+}
+
+module.exports = { redirect };
