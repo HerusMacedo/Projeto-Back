969486b2840fd932116dbf19c8859131e0330ba6
Herus Macedo <herus2019@hotmail.com>
2022-02-02 16:19:24 -0300

    Função DELETE

--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -18,4 +18,8 @@ res.render("index", {error:false, body: {}} )
 
 router.post('/', express.urlencoded({extended: true}), linkController.addLink);
 
+router.delete("/:id", linkController.deleteLink)
+
+router.delete("/",express.json(), linkController.deleteLink)
+
 module.exports = router;
